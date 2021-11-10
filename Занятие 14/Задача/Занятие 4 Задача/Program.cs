0e3b9f89fb716a814578e677d35d77f2b3bc8f91
s.fedorovich <s.fedorovich@CZC71776GT>
2021-11-10 15:55:41 +0300

    Задача

--- Занятие 14/Задача/Занятие 4 Задача/Program.cs
+++ Занятие 14/Задача/Занятие 4 Задача/Program.cs
@@ -9,69 +9,59 @@ namespace Занятие_14_Задача
 {
     abstract class Animal//абстрактный класс животные
     {
-        public Animal (string name, string voice)
-        {
-            Name = name;
-            Voice = voice;            
-        }
-        
         abstract public string Name { get; set; } // конструктор, устанавливающий название животного по умолчанию
         public string Voice; // звук животного
         public void ShowInfo() //неабстрактный метод
         {
-            Console.WriteLine("{0}.{1}", Name, Voice);
+            Console.WriteLine("{0}", Name);
+            Say();
         }
         public abstract void Say(); //абстрактный метод, который выводит звук животного
     }
     class Cat : Animal // класс кошка
     {
-        string name;      
-        public Cat(string name, string voice)
-            : base(name,voice)
-        {
-            this.Name = name;
-            this.Voice = voice;
-        }
+        string name;
+
         public override string Name
         {
-            get { return Name; }
-            set { Name = value; }
+            get { return name; }
+            set { name = value; }
         }
         public override void Say()
         {
-            Console.WriteLine("Животное {0} издает звук: {1}", Name, Voice);
+            Console.WriteLine("Животное {0} издает звук: ", Name); 
+            Say();
+            
         }
     }
     class Dog : Animal // класс собака
     {
         string name;
-        public Dog(string name, string voice)
-          : base(name,voice)
-        {
-             this.Name = name;
-            this.Voice = voice;
-        }
         public override string Name
         {
-            get { return Name; }
-            set { Name = value; }
+            get { return name; }
+            set { name = value; }
         }
 
         public override void Say()
         {
-            Console.WriteLine("Животное {0} издает звук: {1}", Name, Voice);
+            Console.WriteLine("Животное {0} издает звук: ", Name);
+            Say();
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Cat cat = new Cat("Кошка", "Мяу"); //экземпляр класса cat
-            Dog dog = new Dog("Собака", "Гав"); //экземпляр класса dog
+            Cat cat = new Cat(); //экземпляр класса cat
+            cat.Name = "Кошка";
+
+            Dog dog = new Dog(); //экземпляр класса dog
+            dog.Name = "Собака";
             Console.ReadLine();
             cat.Say();
             dog.Say();
-            
+
             Console.ReadKey();
         }
     }
